beecac14d07103f70b467b2d05a5a54f3c2875ba
nmsgh <36178473+nmsgh@users.noreply.github.com>
2018-11-13 15:19:12 -0800

    Added Simulator struct, WorkDone struct for passing completed work, and Euler-Cromer integration.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,36 @@ struct Simulator {
     time : f64
 }
 
+struct WorkDone {
+    force : [f64 ; 3],
+    body_index : usize
+}
+
+impl Simulator {
+    fn compute_force(&self, origin : &Body, attractor : &Body) -> [f64; 3] {
+        let distance : f64 = vector_magnitude(&vector_difference(&attractor.position, &origin.position));
+        let force_magnitude = GRAVITATIONAL_CONSTANT * origin.mass * attractor.mass / distance.powi(2);
+        let rhat = normalize(&vector_difference(&origin.position, &attractor.position));
+
+        vector_scalar_multiple(&rhat, &force_magnitude)
+    }
+
+    fn step_forward(&mut self, work : &mut Vec<WorkDone>) {
+        for i in 0..work.len() {
+            let index = work[i].body_index;
+            let body = &mut self.bodies[index];
+            euler_cromer_integrate(body, &work[i].force, &self.time_step);
+        }
+        self.time += self.time_step;
+    }
+}
+
+fn euler_cromer_integrate(body : &mut Body, force : &[f64 ; 3], time_step : &f64) {
+    let acceleration : [f64 ; 3] = vector_scalar_multiple(&force, &body.mass.powi(-1));
+    body.velocity = vector_sum(&body.velocity, &vector_scalar_multiple(&acceleration, &time_step));
+    body.position = vector_sum(&body.position, &vector_scalar_multiple(&body.velocity, &time_step));
+}
+
 fn vector_scalar_multiple(vector : &[f64 ; 3], scalar : &f64) -> [f64 ; 3] {
     let mut multiple : [f64 ; 3] = [0.0, 0.0, 0.0];
     for index in 0..3 {
