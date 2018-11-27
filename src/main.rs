59bad8dbf7b13c2c1b2f6d5656e80da17bd554bb
Dylan <dylan.dvorachek@gmail.com>
2018-11-27 10:21:38 -0800

    added example of working threadpool with physics simulator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 extern crate thread_pool;
 use thread_pool::ThreadPool;
@@ -13,12 +14,14 @@ fn main() {
 
     // TESTING PHYSICS IMPORTS
     let earth = Body {
+        id : 0,
         position : [149597900000.0, 0.0, 0.0],
         velocity : [0.0, 29800.0, 0.0],
         mass : 5.972e24
     };
 
     let sun = Body {
+        id : 1,
         position : [0.0, 0.0, 0.0],
         velocity : [0.0, 0.0, 0.0],
         mass : 1.989e30
@@ -26,20 +29,43 @@ fn main() {
 
     let bodies : Vec<Body> = vec![sun, earth];
 
-    let mut sim = Simulator {
-        bodies : bodies,
-        time : 0.0,
-        time_step : 60.0
-    };
+    let mut sim = Simulator::new(bodies, 0.0, 60.0);
 
-    // TESTING THREADPOOL IMPORTS
-    //let pool = ThreadPool::new(5);  // this is commented to avoid panick without loop
+    let pool = ThreadPool::new(5);
 
-    // TODO: ADD EXAMPLE OF SIMULATOR USEAGE
-    // - made Simulator, Body and WorkDone structs pub along with attributes
-    //   might have to do same to Simulator functions
-    //   see thread_pool/src/lib.rs for example
+    // CHANNELS FOR RETURNING VALUES FROM THREADPOOL
+    let (tx, rx) = mpsc::channel();
 
     // TODO: RUN SIMULATOR WITH THREADPOOL
+    for _ in 0..24 {
+        let tx1 = mpsc::Sender::clone(&tx);
+        let mut cl = sim.clone();
+
+        pool.execute(move || {
+            let mut val = cl.do_work();
+            tx1.send(val).unwrap();
+        });
+    }
+
+    for _ in 0..24 {
+        let mut received = rx.recv().unwrap();
+        sim.step_forward(&mut received);
+        println!("sim time: {}", sim.time);
+    }
+
+    // TODO: ADD DECONSTRUCTOR FOR THREADPOOL
+    loop{
+        /*
+        pool.execute(|| {
+            wait();
+        });
+        */
+    }
+}
+
+fn wait() {
+    println!("testing wait");
+    thread::sleep(Duration::from_secs(4));
+    println!("wait done!");
 }
 
