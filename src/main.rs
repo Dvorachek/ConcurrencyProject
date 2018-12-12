d1db8dff323f53352e26165991e68cb4cbf40ebe
Dvorachek <Dylan.Dvorachek@gmail.com>
2018-12-12 10:01:26 -0800

    refactored generate_bodies

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,18 @@ extern crate piston_window;
 extern crate find_folder;
 use piston_window::*;
 
-const AU: f64 = 149.6e9; // Astronomical Unit in meters, roughly distance earth -> sun
+const AU: f64 = 1.49597e11;
 const SCALE: f64 = 50.0 / AU;
 const DIMENSION: u32 = 1000;
-const HALF: f64 = AU * 10.0;
+const DAY: f64 = 24.0 * 3600.0;
 
 fn main() {
-
     // INIT SIMULATOR AND THREADPOOL
     let mean_stars = 2.0;
     let mean_planets = 10.0;
     let mean_others = 20.0;
     let bodies = generate_bodies(mean_stars, mean_planets, mean_others);
-    let mut sim = Simulator::new(bodies, 0.0, 10000.0);
+    let mut sim = Simulator::new(bodies, 0.0, DAY);
     let pool = ThreadPool::new(computers_init());
 
     // CHANNELS FOR RETURNING VALUES FROM THREADPOOL
@@ -42,18 +41,17 @@ fn main() {
             },
         ).exit_on_esc(true).build().unwrap();
 
-    // piston window lazy means that only events will tricker a step
     window.set_lazy(false);
 
     // LOOP DRAWING
     while let Some(e) = window.next() {
 
-        let chunk_size = 2;
+        let chunk_size = 2000;
         let mut bodies = sim.bodies.clone();
 
         // DRAW HERE
         window.draw_2d(&e, |c, g| {
-            clear([0.129, 0.1468, 0.168, 1.0], g); // ?????
+            clear([0.129, 0.1468, 0.168, 1.0], g);
             g.clear_stencil(0);
             
             // Compute work
@@ -80,7 +78,7 @@ fn main() {
 
             // Step simulation forward in time
             sim.step_forward(&work_done);
-            println!("sim time: {}", sim.time);
+            //println!("sim time: {}", sim.time);
 
             for body in &sim.bodies {
                 draw_body(&body, c, g);
@@ -90,15 +88,14 @@ fn main() {
 }
 
 fn draw_body(body: &Body, c: Context, g: &mut G2d) {
-    Ellipse::new(body.colour) // colour
+    Ellipse::new(body.colour)
         .draw(
-            [body.position[0] * SCALE + 500.0, body.position[1] * SCALE + 500.0,
-                body.radius, body.radius],  // radius radius
+            [body.position[0] * SCALE + 500.0, body.position[1] * SCALE + 500.0,  // X, Y
+                body.radius, body.radius],
             &c.draw_state, c.transform, g
         );
 }
 
-// PROBABLY OVERKILL, BUT COULD MAKE PUT IN -> impl Computer {
 fn computers_init() -> Vec<Computer> {
 
     let cpu1 = Computer {
@@ -132,84 +129,45 @@ fn computers_init() -> Vec<Computer> {
 
 fn generate_bodies(mean_stars: f64, mean_planets: f64, mean_others: f64) -> Vec<Body> {
     let mut bodies : Vec<Body> = Vec::new();
-    let stellar_mass : f64 = 1.989e30;
-    let au : f64 = 1.49597e11;
-    let mut rng = rand::thread_rng();
 
-    // Generate stars
-    let mut exp = Exp::new(mean_stars.powf(-1.0));
-    let number_of_stars = exp.sample(&mut rand::thread_rng()).ceil() as usize;
+    // ADJUST PARAM HERE
+    let stellar_mass : f64 = 1.989e30 * 5.0;
+    let stellar_v_scale : f64 = 3000.0;
+    let stellar_dist_scale : f64 = 1.0;
+    let stellar_colour : [f32; 4] = [255.0, 255.0, 0.0, 1.0];
+    let stellar_radius : f64 = 15.0;
+    
+    let earth_mass : f64 = 5.972e24 * 50.0;
+    let planet_v_scale : f64 = 30000.0;
+    let planet_dist_scale : f64 = 8.0;
+    let planet_colour : [f32; 4] = [0.69803, 0.186215, 0.12549, 1.0];
+    let planet_radius : f64 = 10.0;
 
-    let mut velocity_scale = 3000.0;
-    for _ in 0..number_of_stars {
-        let x : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-        let y : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-        let z : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * au;
+    let other_mass : f64 = 2.0e15 * 5.0e5;
+    let other_v_scale : f64 = 300.0;
+    let other_dist_scale : f64 = 16.0;
+    let other_colour : [f32; 4] = [0.298039, 0.7705882, 0.411765, 1.0];
+    let other_radius : f64 = 8.0;
 
-        let vx : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-        let vy : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-        let vz : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-
-        let body = Body {
-            id : 0,
-            position : [x, y, z],
-            velocity : [vx, vy, vz],
-            mass : 5.0 * exp.sample(&mut rand::thread_rng()) * stellar_mass,
-            colour : [255.0, 255.0, 0.0, 1.0],
-            radius : 15.0,
-        };
-        bodies.push(body);
-    }
-
-    // Generate planets
-    exp = Exp::new(mean_planets.powf(-1.0));
-    let earth_mass : f64 = 5.972e24;
-    let number_of_planets = exp.sample(&mut rand::thread_rng()).ceil() as usize;
-    velocity_scale = 30000.0;
-    for _ in 0..number_of_planets {
-        let x : f64 = 8.0 * (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-        let y : f64 = 8.0 * (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-        let z : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-
-        let vx : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-        let vy : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-        let vz : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-
-        let body = Body {
-            id : 0,
-            position : [x, y, z],
-            velocity : [vx, vy, vz],
-            mass : 50.0 * exp.sample(&mut rand::thread_rng()) * earth_mass,
-            colour : [0.69803, 0.186215, 0.12549, 1.0],
-            radius : 10.0
-        };
-        bodies.push(body);
-    }
-
-    // Generate small bodies
-    exp = Exp::new(mean_others.powf(-1.0));
-    let other_mass : f64 = 2.0e15;
-    let number_of_other = exp.sample(&mut rand::thread_rng()).ceil() as usize;
-    velocity_scale = 300.0;
-    for _ in 0..number_of_other {
-        let x : f64 = 16.0 * (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-        let y : f64 = 16.0 * (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-        let z : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * au;
-
-        let vx : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-        let vy : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-        let vz : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
-
-        let body = Body {
-            id : 0,
-            position : [x, y, z],
-            velocity : [vx, vy, vz],
-            mass : 5.0e5 * exp.sample(&mut rand::thread_rng()) * other_mass,
-            colour : [0.298039, 0.7705882, 0.411765, 1.0],
-            radius : 8.0,
-        };
-        bodies.push(body);
-    }
+    
+    bodies.append(&mut make_body_vec(stellar_mass,
+        mean_stars,
+        stellar_v_scale,
+        stellar_dist_scale,
+        stellar_colour,
+        stellar_radius));
+    bodies.append(&mut make_body_vec(earth_mass,
+        mean_planets,
+        planet_v_scale,
+        planet_dist_scale,
+        planet_colour,
+        planet_radius));
+    bodies.append(&mut make_body_vec(other_mass,
+        mean_others,
+        other_v_scale,
+        other_dist_scale,
+        other_colour,
+        other_radius));
 
     // Ensure high mass bodies are near the end
     bodies.reverse();
@@ -218,5 +176,35 @@ fn generate_bodies(mean_stars: f64, mean_planets: f64, mean_others: f64) -> Vec<
         body.id = id.clone();
         id += 1;
     }
+    bodies
+}
+
+fn make_body_vec(mass: f64, mean: f64, velocity_scale: f64, distance_scale: f64, colour: [f32; 4], radius: f64) -> Vec<Body> {
+    let mut bodies : Vec<Body> = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    let exp = Exp::new(mean.powf(-1.0));
+    let number_of_bodies = exp.sample(&mut rand::thread_rng()).ceil() as usize;
+
+    for _ in 0..number_of_bodies {
+        let x : f64 = distance_scale * (-1.0 + 2.0 * rng.gen::<f64>()) * AU;
+        let y : f64 = distance_scale * (-1.0 + 2.0 * rng.gen::<f64>()) * AU;
+        let z : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * AU;
+
+        let vx : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
+        let vy : f64 = (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
+        let vz : f64 = 0.01 * (-1.0 + 2.0 * rng.gen::<f64>()) * velocity_scale;
+
+        let body = Body {
+            id : 0,
+            position : [x, y, z],
+            velocity : [vx, vy, vz],
+            mass : exp.sample(&mut rand::thread_rng()) * mass,
+            colour : colour,
+            radius : radius,
+        };
+        bodies.push(body);
+    }
+
     bodies
 }
